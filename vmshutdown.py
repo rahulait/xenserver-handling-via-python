dd0555768c4e8cbb2221ddd705fac8e58963108f
rahulsharmaait@gmail.com <rahulsharmaait@gmail.com@5f899ac3-3dd9-314d-38d8-28c129460880>
2011-07-12 11:42:52 +0000

    script to shutdown VM whose name is provided.....

---
+++ vmshutdown.py
@@ -0,0 +1,53 @@
+'''
+Created on Jul 12, 2011
+
+@author: Rahul Sharma
+'''
+
+import sys, time, string
+
+import XenAPI
+
+
+def main(session,vm_name):
+    # Find a non-template VM object
+    vms_all=session.xenapi.VM.get_all()
+    print "XenServer has %d VMs which also includes templates....." % len(vms_all)
+    
+    for vm in vms_all:
+        record = session.xenapi.VM.get_record(vm)
+        
+        # We cannot power-cycle templates and we should avoid touching control domains
+        # unless we are really sure of what we are doing...
+        if not(record["is_a_template"]) and not(record["is_control_domain"]):
+            name = record["name_label"]
+            flag=cmp(vm_name,name)
+            if not flag:
+                powerstate=record["power_state"]
+                print "VM %s is in power state '%s'" % (name,powerstate)
+                chkpowstate=cmp(string.upper(powerstate),"RUNNING")
+                if not(chkpowstate) :
+                    print "Found VM uuid", record["uuid"], "called: ", name
+                    print "Shutting down the VM...."
+                    try:
+                        session.xenapi.VM.clean_shutdown(vm)
+                    except XenAPI.Failure as err :
+                        print "%s" % err
+                else:
+                    print "VM with uuid %s not in running state, so no change to it" % record["uuid"]
+    print "Program Ended....."
+
+                
+if __name__ == "__main__":
+    if len(sys.argv) <> 5:
+        print "Usage:"
+        print sys.argv[0], " <url> <username> <password> <Name of VM to start>"
+        sys.exit(1)
+    url = sys.argv[1]
+    username = sys.argv[2]
+    password = sys.argv[3]
+    vm_name=sys.argv[4]
+    # First acquire a valid session by logging in:
+    session = XenAPI.Session(url)
+    session.xenapi.login_with_password(username, password)
+    main(session,vm_name)
